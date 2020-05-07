357a78e6ac49dd470d2d2258696a330e3d281fb9
aryan0078 <aryan8931080555@gmail.com>
2020-05-07 15:08:01 +0530

    Done API

--- index.js
+++ index.js
@@ -1,23 +1,17 @@
 const youtube = require('scrape-youtube').default;
  const youtubedl = require('youtube-dl')
-youtube.search('Short Change Hero').then(results => {
-   // console.log()
-    url(results[0].link)
-});
-function url(x){
-const url = x
-// Optional arguments passed to youtube-dl.
 
- 
-youtubedl.getInfo(url, function(err, info) {
-  if (err) throw err
- 
-  console.log('id:', info.id)
-  console.log('title:', info.title)
-  console.log('url:', info.url)
-  console.log('thumbnail:', info.thumbnail)
-  console.log('description:', info.description)
-  console.log('filename:', info._filename)
-  console.log('format id:', info.format_id)
+
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+ youtube.search('lean on').then(results => {
+	
+res.send(results[0].id)
+});
 })
-}
+
+app.listen(port, () => console.log(`Soul Api on:${port}`))
